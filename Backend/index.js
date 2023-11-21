787bf6e57bfae4443890112699389cc72bcd4c98
Zaynab Ramou <zramou25@wooster.edu>
2023-11-21 18:04:18 -0500

    Optimized for final deployment

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require('cors');
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const path = require('path'); // Added line
 const app = express();
 const pinRoute = require("./routes/pins");
 const userRoute = require("./routes/users");
@@ -23,6 +24,14 @@ mongoose
 
 app.use("/api/pins", pinRoute);
 app.use("/api/users", userRoute);
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')));  // Replace with your path
+
+// Anything that doesn't match the above, send back the frontend's index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));  // Replace with your path
+});
 
 app.listen(8800, () => {
   console.log("Backend server is running on port 8800!");
