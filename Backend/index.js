cae6babbd257f8ed750832fb1a90637e66282656
Zaynab Ramou <zramou25@wooster.edu>
2023-11-21 12:37:35 -0500

    optimize for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const cors = require('cors');
-const mongoose = require ("mongoose");
-const dotenv = require ("dotenv");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
 const pinRoute = require("./routes/pins");
 const userRoute = require("./routes/users");
 
 dotenv.config();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
 mongoose
-.connect( process.env.MONGO_URL, {
-    useNewUrlParser: true, //Corrected here
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => {
-    console.log("MongoDB Connected")
-})
-.catch(err => console.log(err));
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch(err => console.log(err));
 
 app.use("/api/pins", pinRoute);
 app.use("/api/users", userRoute);
 
-app.listen( 8800, ()=> {
-    console.log("Backend server is running!")
+app.listen(8800, () => {
+  console.log("Backend server is running on port 8800!");
 });
